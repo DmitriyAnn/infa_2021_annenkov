034c04dced741309e79ca925d1092aa5aac80783
Alexey <alexey@MacBook-Pro.local>
2021-10-01 10:57:48 +0300

    Исправлены все недостатки

--- lab3/task2.py
+++ lab3/task2.py
@@ -4,8 +4,8 @@ from pygame.draw import *
 
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
-pygame.display.update()
 clock = pygame.time.Clock()
+
 #задаём нужные цвета в RGB
 green_light = (34, 224, 0)
 green_dark = (0, 77, 0)
@@ -17,9 +17,10 @@ pink_light = (255, 140, 157)
 pink_dark = (255, 41, 77)
 white = (255, 255, 255)
 black = (0, 0, 0)
-#рисуем землю и небо
-ground = polygon(screen, green_light, [(0, 200), (0, 600), (800, 600), (800, 200)], 0)
-sky = polygon(screen, blue_light, [(0, 0), (800, 0), (800, 200), (0, 200)], 0)
+
+# Переменные для рисования солнца
+R = 50
+dr = 5
 
 
 def house(x, y, height, width):
@@ -31,24 +32,24 @@ def house(x, y, height, width):
         screen,
         brown_light,
         [(x, y), (x + width, y), (x + width, y - height), (x, y - height)],
-        0,
+        0
     )
     house_border = polygon(
         screen,
         (0, 0, 0),
         [(x, y), (x + width, y), (x + width, y - height), (x, y - height)],
-        1,
+        1
     )
     house_hat = polygon(
         screen,
         pink_dark,
-        [(x, y - height), (x + width, y - height), (x + width / 2, y - 3 * height / 2)],
+        [(x, y - height), (x + width, y - height), (x + width / 2, y - 3 * height / 2)]
     )
     house_hat_border = polygon(
         screen,
         (0, 0, 0),
         [(x, y - height), (x + width, y - height), (x + width / 2, y - 3 * height / 2)],
-        1,
+        1
     )
     window = polygon(
         screen,
@@ -57,9 +58,9 @@ def house(x, y, height, width):
             (x + 3 * width / 8, y - 3 * height / 9),
             (x + 5 * width / 8, y - 3 * height / 9),
             (x + 5 * width / 8, y - 6 * height / 9),
-            (x + 3 * width / 8, y - 6 * height / 9),
+            (x + 3 * width / 8, y - 6 * height / 9)
         ],
-        0,
+        0
     )
     window_border = polygon(
         screen,
@@ -68,9 +69,9 @@ def house(x, y, height, width):
             (x + 3 * width / 8, y - 3 * height / 9),
             (x + 5 * width / 8, y - 3 * height / 9),
             (x + 5 * width / 8, y - 6 * height / 9),
-            (x + 3 * width / 8, y - 6 * height / 9),
+            (x + 3 * width / 8, y - 6 * height / 9)
         ],
-        1,
+        1
     )
 
 
@@ -98,8 +99,8 @@ def tree(x, y, scale):
             (x, y),
             (x + scale * 20, y),
             (x + scale * 20, y - scale * 100),
-            (x, y - scale * 100),
-        ],
+            (x, y - scale * 100)
+        ]
     )
     d = 0
     h = 0
@@ -118,63 +119,6 @@ def tree(x, y, scale):
         c += 60 * scale
 
 
-'''R1 = 40
-R2 = 60
-k = 0.0175
-a1 = 67.5
-a2 = 22.5
-
-
-def sun(x, y, color):
-    polygon(
-        screen,
-        color,
-        [
-            (x, y - R1),
-            (x + R2 * np.cos(k * a1), y - R2 * np.sin(k * a1)),
-            (x + R1 * np.cos(k * 45), y - R1 * np.sin(k * 45)),
-            (x + R2 * np.cos(k * a2), y - R2 * np.sin(k * a2)),
-            (x + R1, y),
-            (x + R2 * np.cos(k * a2), y + R2 * np.sin(k * a2)),
-            (x + R1 * np.cos(k * 45), y + R1 * np.sin(k * 45)),
-            (x + R2 * np.cos(k * a1), y + R2 * np.sin(k * a1)),
-            (x, y + R1),
-            (x - R2 * np.cos(k * a1), y + R2 * np.sin(k * a1)),
-            (x - np.cos(k * 45) * R1, y + np.sin(k * 45) * R1),
-            (x - R2 * np.cos(k * a2), y + R2 * np.sin(k * a2)),
-            (x - R1, y),
-            (x - R2 * np.cos(k * a2), y - R2 * np.sin(k * a2)),
-            (x - np.cos(k * 45) * R1, y - np.sin(k * 45) * R1),
-            (x - R2 * np.cos(k * a1), y - R2 * np.sin(k * a1)),
-        ],
-    )
-
-    polygon(
-        screen,
-        black,
-        [
-            (x, y - R1),
-            (x + R2 * np.cos(k * a1), y - R2 * np.sin(k * a1)),
-            (x + R1 * np.cos(k * 45), y - R1 * np.sin(k * 45)),
-            (x + R2 * np.cos(k * a2), y - R2 * np.sin(k * a2)),
-            (x + R1, y),
-            (x + R2 * np.cos(k * a2), y + R2 * np.sin(k * a2)),
-            (x + R1 * np.cos(k * 45), y + R1 * np.sin(k * 45)),
-            (x + R2 * np.cos(k * a1), y + R2 * np.sin(k * a1)),
-            (x, y + R1),
-            (x - R2 * np.cos(k * a1), y + R2 * np.sin(k * a1)),
-            (x - np.cos(k * 45) * R1, y + np.sin(k * 45) * R1),
-            (x - R2 * np.cos(k * a2), y + R2 * np.sin(k * a2)),
-            (x - R1, y),
-            (x - R2 * np.cos(k * a2), y - R2 * np.sin(k * a2)),
-            (x - np.cos(k * 45) * R1, y - np.sin(k * 45) * R1),
-            (x - R2 * np.cos(k * a1), y - R2 * np.sin(k * a1)),
-        ],
-        1,
-    )
-'''
-R = 50
-dr = 5
 def sun(x,y,n):
     '''Функция рисует солнышко, параметры:
     x,y - координаты центра
@@ -187,21 +131,29 @@ def sun(x,y,n):
     polygon(screen, pink_light, m)
     polygon(screen, black, m, 1)
 
-house(100, 400, 150, 200)
-house(500, 350, 90, 120)
+def draw_all():
+    # рисуем землю и небо
+    ground = polygon(screen, green_light, [(0, 200), (0, 600), (800, 600), (800, 200)], 0)
+    sky = polygon(screen, blue_light, [(0, 0), (800, 0), (800, 200), (0, 200)], 0)
 
-clouds(450, 100, white, 15)
-clouds(600, 75, white, 20)
-clouds(300, 75, white, 20)
+    house(100, 400, 150, 200)
+    house(500, 350, 90, 120)
 
-tree(350, 350, 1)
-tree(670, 300, 0.6)
+    clouds(450, 100, white, 15)
+    clouds(600, 75, white, 20)
+    clouds(300, 75, white, 20)
 
-sun(80, 80, 80)
+    tree(350, 350, 1)
+    tree(670, 300, 0.6)
 
-pygame.display.update()
+    sun(80, 80, 80)
+
+
 while True:
-    clock.tick(30)
+    screen.fill(black)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+
+    draw_all()
+    pygame.display.update()
